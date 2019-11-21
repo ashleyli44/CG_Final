fa89e481d1d372e704c4913d208ca07ec404e195
skambha6 <51316814+skambha6@users.noreply.github.com>
2019-11-21 18:02:04 -0500

    Update bloom_factory.py

--- bloom_factory.py
+++ bloom_factory.py
@@ -1,3 +1,56 @@
 '''
 Bloom filter factory
 '''
+
+##Warning: unfinalized/untested code
+
+##READ ME: install mmh3 and bitarray via pip install mmh3 and pip install bitarray
+import math
+import mmh3
+from bitarray import bitarray
+
+class BloomFilterFactory(object):
+    def __init__(self, kmer_list, fpr):
+        self.kmer_count = size(kmer_list)
+        self.fpr = fpr
+        self.BFsize = self.getBFsize(self.kmer_count, self.fpr)
+        self.hashFunctionCount = self.get_hashFunctionCount(self.kmer_count, self.BFSize)
+
+        self.BF = bitarray(self.m)
+        self.BF.setall(0)
+
+        for kmer in kmer_list:
+            self.add(kmer)
+
+    @classmethod
+    def getBFsize(self,n,fpr):
+        ##figure out math behind this
+        m = -(n * math.log(fpr)) / (math.log(2) ** 2)
+        return int(m)
+
+    @classmethod
+    def get_hashFunctionCount(self, m, n):
+        k = (m / n) * math.log(2)
+        return int(k)
+
+    ##make this public method?
+    @classmethod
+    def add(self, kmer):
+        #hashes = []
+        for i in range(self.hashFunctionCount):
+            hashValue = mmh3.hash(kmer, i) % self.BFsize
+            #hashes.append(hashValue)
+            self.BF[hashValue] = True
+
+
+    def checkExistence(self, item):
+        for i in range(self.hashFunctionCount):
+            hashValue = mmh3.hash(item, i) % self.BFsize
+            if self.BF[hashValue] == False:
+                return False
+            return True
+
+    def returnBF(self):
+        return self.bit_array
+
+
