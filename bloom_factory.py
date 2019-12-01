2bce874e99fc9989414559ea865e707c6d7d6234
skambha6 <51316814+skambha6@users.noreply.github.com>
2019-11-30 19:08:29 -0800

    Update bloom_factory.py

--- bloom_factory.py
+++ bloom_factory.py
@@ -2,24 +2,25 @@
 Bloom filter factory
 """
 
-# Warning: unfinalized/untested code
-
 # READ ME: install mmh3 and bitarray via pip install mmh3 and pip install bitarray
 import math
 import mmh3
 from bitarray import bitarray
 
-from bloom_filter import BloomFilter
+import numpy as np
+
+#from bloom_filter import BloomFilter
 
 
 class BloomFilterFactory(object):
+    
+    #kmer_list should be a list, fpr should be a double
     def __init__(self, kmer_list, fpr):
-        self.kmer_count = size(kmer_list)
+        self.kmer_count = len(kmer_list)
         self.fpr = fpr
         self.BFsize = self.getBFsize(self.kmer_count, self.fpr)
-        self.hashFunctionCount = self.get_hashFunctionCount(self.kmer_count, self.BFSize)
-
-        self.BF = bitarray(self.m)
+        self.hashFunctionCount = self.get_hashFunctionCount(self.kmer_count, self.BFsize)
+        self.BF = bitarray(self.BFsize)
         self.BF.setall(0)
 
         for kmer in kmer_list:
@@ -27,22 +28,18 @@ class BloomFilterFactory(object):
 
     @classmethod
     def getBFsize(self,n,fpr):
-        ##figure out math behind this
         m = -(n * math.log(fpr)) / (math.log(2) ** 2)
         return int(m)
 
     @classmethod
-    def get_hashFunctionCount(self, m, n):
+    def get_hashFunctionCount(self, n, m):
         k = (m / n) * math.log(2)
         return int(k)
 
-    @classmethod
     def add(self, kmer):
-        # hashes = []
-        for i in range(self.hashFunctionCount):
-            hashValue = mmh3.hash(kmer, i) % self.BFsize
-            # hashes.append(hashValue)
-            self.BF[hashValue] = True
+    	for i in range(self.hashFunctionCount):
+          hashValue = mmh3.hash(kmer, i) % self.BFsize
+          self.BF[hashValue] = True
 
     def checkExistence(self, item):
         for i in range(self.hashFunctionCount):
