d3945b5523f68ab7768f7531f7cff7c17a2f1aed
skambha6 <51316814+skambha6@users.noreply.github.com>
2019-12-04 17:30:51 -0500

    Update bloom_filter.py

--- bloom_filter.py
+++ bloom_filter.py
@@ -3,13 +3,20 @@ Bloom filter class
 """
 
 import mmh3
-
+from bitarray import bitarray
 
 class BloomFilter:
-    def __init__(self, str_id, b_vect, h):
+    def __init__(self, str_id, kmer_list, bf_size, h):
         self.name = str_id  # string name
-        self.bvector = b_vect  # bitarray vector that represents the bloom filter
+        self.kmer_count = len(kmer_list)
+        self.BFsize = bf_size 
+        self.BF = bitarray(self.BFsize) # bitarray vector that represents the bloom filter
+        #self.BF.setall(0)
+          
         self.h = h  # number of hash functions to use per entry
+        
+        for kmer in kmer_list:
+            self.add(kmer)
     
     def compareTo(self, bf):
         """
@@ -23,12 +30,12 @@ class BloomFilter:
 
     def add(self, kmer):
         for i in range(self.h):
-            hashValue = mmh3.hash(kmer, i) % self.bvector.length()
+            hashValue = mmh3.hash(kmer, i) % self.BF.length()
             self.bvector[hashValue] = True
 
     def contains(self, kmer):
         for i in range(self.h):
-            hashValue = mmh3.hash(kmer, i) % self.bvector.length()
+            hashValue = mmh3.hash(kmer, i) % self.BF.length()
             if not self.bvector[hashValue]:
                 return False
         return True
@@ -40,3 +47,6 @@ class BloomFilter:
             self.name = self.name + "U" + other_bloom_filter.name
         for i in range(len(self.bvector)):
             self.bvector[i] = self.bvector[i] or other_bloom_filter.bvector[i]
+            
+    def getBF(self):
+        return self.BF
